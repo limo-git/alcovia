abe231ff5204ce398be13d33973d76371232e9cc
limo-git <Dimpleyadav32@gmail.com>
2024-12-25 13:00:56 +0530

    eslint2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,14 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = {
   extends: [
-    ...compat.extends("next/core-web-vitals", "next/typescript")
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
   ],
   rules: {
-    "@typescript-eslint/no-unused-vars": ["off"],
-    "@typescript-eslint/no-explicit-any": ["off"],
-    "@typescript-eslint/ban-ts-comment": ["off"],
-    "prefer-const": ["off"],
-    "react-hooks/exhaustive-deps": ["off"]
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "prefer-const": "off",
+    "react-hooks/exhaustive-deps": "off",
   },
 };
 
